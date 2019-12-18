3fa3526541d959bd40ca81788af669b868028aac
avnish kumar <awi.singh@hotmail.com>
2019-12-18 17:34:42 +0530

    Update mongodb.js

--- judge/utils/database/mongodb.js
+++ judge/utils/database/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/OnlineJudge')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://debanjan_01:deb12345@firstcluster-yy6sf.mongodb.net/computeaid?retryWrites=true&w=majority')
 module.exports = {
   mongoose
 }
